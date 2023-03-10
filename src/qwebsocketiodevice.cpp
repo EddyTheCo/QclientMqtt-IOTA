434c606340e8b108071415eb8acf75b9fec82eb7
EddyGlezz <eddyglezz95@gmail.com>
2023-03-10 19:49:10 +0100

    change deprecated error function

--- src/qwebsocketiodevice.cpp
+++ src/qwebsocketiodevice.cpp
@@ -9,7 +9,7 @@ WebSocketIODevice::WebSocketIODevice(QObject *parent)
     qDebug()<<"WebSocketIODevice";
     connect(&m_socket, &QWebSocket::connected, this, &WebSocketIODevice::onSocketConnected);
     connect(&m_socket, &QWebSocket::binaryMessageReceived, this, &WebSocketIODevice::handleBinaryMessage);
-    connect(&m_socket,QOverload<QAbstractSocket::SocketError>::of(&QWebSocket::error),this,
+    connect(&m_socket,QOverload<QAbstractSocket::SocketError>::of(&QWebSocket::errorOccurred),this,
             [=](QAbstractSocket::SocketError error)
     {
         qDebug()<<"error websocket:"<<error;
