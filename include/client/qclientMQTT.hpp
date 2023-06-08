d6c4d68b2e10b927b6112328e9ce024d3ef17271
EddyGlezz <eddyglezz95@gmail.com>
2023-06-08 11:50:43 +0200

    fix windows shared library

--- include/client/qclientMQTT.hpp
+++ include/client/qclientMQTT.hpp
@@ -13,50 +13,50 @@
 
 namespace qiota{
 
-class ResponseMqtt: public QObject
-{
-    Q_OBJECT
-public:
-    ResponseMqtt(QMqttSubscription * thesub);
-public slots:
-    void fill(QMqttMessage msg);
-    void unsubscribe(void);
-signals:
-    void returned( QJsonValue data );
-private:
-    QMqttSubscription * sub_;
+	class QCMQTT ResponseMqtt: public QObject
+	{
+		Q_OBJECT
+		public:
+			ResponseMqtt(QMqttSubscription * thesub);
+			public slots:
+				void fill(QMqttMessage msg);
+			void unsubscribe(void);
+		signals:
+			void returned( QJsonValue data );
+		private:
+			QMqttSubscription * sub_;
 
-};
+	};
 
 
-class ClientMqtt: public QMqttClient
-{
+	class QCMQTT ClientMqtt: public QMqttClient
+	{
 
-    Q_OBJECT
-public:
-    ClientMqtt(QObject *parent = nullptr);
+		Q_OBJECT
+		public:
+			ClientMqtt(QObject *parent = nullptr);
 
-    ResponseMqtt* get_subscription(const QString& topic);
-    ResponseMqtt* get_outputs_unlock_condition_address(const QString& condition_address);
+			ResponseMqtt* get_subscription(const QString& topic);
+			ResponseMqtt* get_outputs_unlock_condition_address(const QString& condition_address);
 
-    template<qblocks::Output::types outtype>
-    ResponseMqtt* get_outputs_by_chain_id(const QString& id)
-    {
-        return get_subscription("outputs/"+qblocks::Output::typesstr[outtype]+"/"+id);
-    }
-    ResponseMqtt* get_outputs_outputId(const QString& outid);
-    ResponseMqtt* get_blocks(void);
-    void set_node_address(const QUrl &url){if(node_address_!=url&&url.isValid()){
-            node_address_=url;emit node_address_changed();}}
+			template<qblocks::Output::types outtype>
+				ResponseMqtt* get_outputs_by_chain_id(const QString& id)
+				{
+					return get_subscription("outputs/"+qblocks::Output::typesstr[outtype]+"/"+id);
+				}
+			ResponseMqtt* get_outputs_outputId(const QString& outid);
+			ResponseMqtt* get_blocks(void);
+			void set_node_address(const QUrl &url){if(node_address_!=url&&url.isValid()){
+				node_address_=url;emit node_address_changed();}}
 
-signals:
-    void node_address_changed();
-private:
-    void restart(void);
-    void openDevice();
-QUrl  node_address_;
-quint16 port_;
-WebSocketIODevice* m_device;
-};
+		signals:
+			void node_address_changed();
+		private:
+			void restart(void);
+			void openDevice();
+			QUrl  node_address_;
+			quint16 port_;
+			WebSocketIODevice* m_device;
+	};
 
 };
--- include/client/qwebsocketiodevice.hpp
+++ include/client/qwebsocketiodevice.hpp
@@ -4,33 +4,39 @@
 #include <QtCore/QIODevice>
 #include <QtWebSockets/QWebSocket>
 #include<QByteArray>
+#include <QtCore/QtGlobal>
 
-class WebSocketIODevice : public QIODevice
+#if defined(WINDOWS_QCMQTT)
+# define QCMQTT_EXPORT Q_DECL_EXPORT
+#else
+#define QCMQTT_EXPORT Q_DECL_IMPORT
+#endif
+
+class QCMQTT WebSocketIODevice : public QIODevice
 {
-    Q_OBJECT
-public:
-    WebSocketIODevice(QObject *parent = nullptr);
+	Q_OBJECT
+	public:
+		WebSocketIODevice(QObject *parent = nullptr);
 
-    bool open(OpenMode mode=QIODevice::ReadWrite) override;
-    void close() override;
+		bool open(OpenMode mode=QIODevice::ReadWrite) override;
+		void close() override;
 
-    qint64 readData(char *data, qint64 maxlen) override;
-    qint64 writeData(const char *data, qint64 len) override;
+		qint64 readData(char *data, qint64 maxlen) override;
+		qint64 writeData(const char *data, qint64 len) override;
 
-    void setUrl(const QUrl &url);
-    void setProtocol(const QByteArray &data);
-Q_SIGNALS:
-    void socketConnected();
+		void setUrl(const QUrl &url);
+		void setProtocol(const QByteArray &data);
+		signals:
+		void socketConnected();
 
-public slots:
-    void handleBinaryMessage(const QByteArray &msg);
-    void onSocketConnected();
+		void handleBinaryMessage(const QByteArray &msg);
+		void onSocketConnected();
 
-private:
-    QByteArray m_protocol;
-    QByteArray m_buffer;
-    QWebSocket m_socket;
-    QUrl m_url;
+	private:
+		QByteArray m_protocol;
+		QByteArray m_buffer;
+		QWebSocket m_socket;
+		QUrl m_url;
 };
 
 #endif // WEBSOCKETIODEVICE_H
