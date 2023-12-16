1ab1ef1ff9c4313fdfc7e0b75493ef8355ca7113
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-12-16 11:30:30 +0100

    Improvements (#28)
    * Add parent to the response that should normally be the client
    
    * Lambda capture this for c++20, there is a bug for macoss on MOC for version lower that qt6.7
    
    * Aligned naming
    
    * Build docs on tags and use versioning for common

--- qclientMQTT.cpp
+++ qclientMQTT.cpp
@@ -5,7 +5,7 @@
 #include<QTimer>
 namespace qiota{
 
-ResponseMqtt::ResponseMqtt(QMqttSubscription * thesub):sub_(thesub)
+ResponseMqtt::ResponseMqtt(QMqttSubscription * thesub,QObject *parent):QObject(parent),sub_(thesub)
 {
     QObject::connect(sub_,&QMqttSubscription::messageReceived ,this, &ResponseMqtt::fill);
 }
@@ -20,7 +20,7 @@ void ResponseMqtt::unsubscribe(void)
 }
 void ClientMqtt::openDevice()
 {
-    m_device->setUrl(node_address_);
+    m_device->setUrl(m_nodeAddress);
     if (!m_device->open(QIODevice::ReadWrite))
     {
         qDebug() << "Could not open socket device";
@@ -37,11 +37,11 @@ void ClientMqtt::restart(void)
 }
 ClientMqtt::ClientMqtt(QObject *parent):QMqttClient(parent),m_device(new WebSocketIODevice(this))
 {
-    QObject::connect(this,&QMqttClient::errorChanged,this,[=](QMqttClient::ClientError error ){
+    QObject::connect(this,&QMqttClient::errorChanged,this,[this](QMqttClient::ClientError error ){
         qDebug()<<"errormqttt:"<<error;
         this->restart();
     });
-    connect(this,&ClientMqtt::node_address_changed,this,[=](){
+    connect(this,&ClientMqtt::nodeAddressChanged,this,[this](){
         this->restart();
     });
     connect(m_device, &WebSocketIODevice::socketConnected, this, [this]() {
@@ -53,8 +53,7 @@ ClientMqtt::ClientMqtt(QObject *parent):QMqttClient(parent),m_device(new WebSock
 
 ResponseMqtt*  ClientMqtt::get_subscription(const QString& topic)
 {
-    auto var=subscribe(QMqttTopicFilter(topic));
-    return new ResponseMqtt(var);
+    return new ResponseMqtt(subscribe(QMqttTopicFilter(topic)),this);
 }
 
 ResponseMqtt* ClientMqtt::get_outputs_unlock_condition_address(const QString& condition_address)
